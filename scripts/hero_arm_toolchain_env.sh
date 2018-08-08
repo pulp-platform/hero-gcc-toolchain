a3c04f547caecc295cdb1e7158ef0f2e5b123c3e
Alessandro Capotondi <alessandro.capotondi@unibo.it>
2018-08-08 23:56:22 +0200

    Reworked arm builder

--- scripts/hero_arm_toolchain_env.sh
+++ scripts/hero_arm_toolchain_env.sh
@@ -15,11 +15,10 @@
 #
 # Authors: Alessandro Capotondi, University of Bologna (alessandro.capotondi@unibo.it)
 #
-# This environmental variables for the GCC 5.2.0 toolchain for the HERO accelerator
+# This environmental variables for the GCC 7.1.1 toolchain for the HERO accelerator
 
 HERO_HOST_TARGET=arm-linux-gnueabihf
 HERO_BUILD_TARGET=x86_64-linux-gnu
-HERO_HOST_USE_NEWLIB=0
 HERO_HOST_LINUX_ARCH=arm
 HERO_HOST_GLIBC=glibc-2.21
 
@@ -59,30 +58,30 @@ HERO_HOST_LINUX_KERNEL_DIR=${HERO_LINUX_KERNEL_DIR}
 export PATH="${HERO_GCC_INSTALL_DIR}/bin":${PATH}
 export LD_LIBRARY_PATH="${HERO_GCC_INSTALL_DIR}/lib":${LD_LIBRARY_PATH}
 
-if [ -z "${HERO_SDK_DIR}" ]; then
-	echo "Error: HERO_SDK_DIR environmental variables is missing!"
-	RET=1
-fi
-if [ -z "${PLATFORM}" ]; then
-	echo "Error: PLATFORM environmental variables is missing!"
-	RET=1
-fi
-if [ -z "${PULP_SDK_INSTALL}" ]; then
-	echo "Error: PULP_SDK_INSTALL environmental variables is missing!"
-	RET=1
-fi
+# if [ -z "${HERO_SDK_DIR}" ]; then
+# 	echo "Error: HERO_SDK_DIR environmental variables is missing!"
+# 	RET=1
+# fi
+# if [ -z "${PLATFORM}" ]; then
+# 	echo "Error: PLATFORM environmental variables is missing!"
+# 	RET=1
+# fi
+# if [ -z "${PULP_SDK_INSTALL}" ]; then
+# 	echo "Error: PULP_SDK_INSTALL environmental variables is missing!"
+# 	RET=1
+# fi
 
-# export PULP_HERO_CPPFLAGS="-I${PULP_SDK_INSTALL}/include/archi/chips/bigpulp -I${PULP_SDK_INSTALL}/include -I${HERO_SDK_DIR}/libpulp-offload/inc -O3 -Wall -g2 -shared -fPIC -DPLATFORM=${PLATFORM}"
-# export PULP_HERO_LDFLAGS="-L${HERO_SDK_DIR}/libpulp-offload/lib -lpulp-offload"
+# # export PULP_HERO_CPPFLAGS="-I${PULP_SDK_INSTALL}/include/archi/chips/bigpulp -I${PULP_SDK_INSTALL}/include -I${HERO_SDK_DIR}/libpulp-offload/inc -O3 -Wall -g2 -shared -fPIC -DPLATFORM=${PLATFORM}"
+# # export PULP_HERO_LDFLAGS="-L${HERO_SDK_DIR}/libpulp-offload/lib -lpulp-offload"
 
-# GCC PULP HERO Libgomp plugin compilation flags
-export LIBGOMP_PLUGIN_PULP_HERO_CPPFLAGS="-O3 -Wall -g2 -shared -fPIC -I${HERO_SDK_DIR}/libpulp-offload/inc -I${PULP_SDK_INSTALL}/include/archi/chips/bigpulp  -I${PULP_SDK_INSTALL}/include -DPLATFORM=${PLATFORM}"
-export LIBGOMP_PLUGIN_PULP_HERO_LDFLAGS="-L${HERO_SDK_DIR}/libpulp-offload/lib -lpulp-offload -lstdc++"
+# # GCC PULP HERO Libgomp plugin compilation flags
+# export LIBGOMP_PLUGIN_PULP_HERO_CPPFLAGS="-O3 -Wall -g2 -shared -fPIC -I${HERO_SDK_DIR}/libpulp-offload/inc -I${PULP_SDK_INSTALL}/include/archi/chips/bigpulp  -I${PULP_SDK_INSTALL}/include -DPLATFORM=${PLATFORM}"
+# export LIBGOMP_PLUGIN_PULP_HERO_LDFLAGS="-L${HERO_SDK_DIR}/libpulp-offload/lib -lpulp-offload -lstdc++"
 
-# MKOFFLOAD external compilation flags
-export PULP_HERO_EXTCFLAGS="-march=rv32imcxpulpv2 -D__riscv__ -DPLATFORM=${PLATFORM} -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include"
+# # MKOFFLOAD external compilation flags
+# export PULP_HERO_EXTCFLAGS="-march=rv32imcxpulpv2 -D__riscv__ -DPLATFORM=${PLATFORM} -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include"
 
-export PULP_HERO_EXTLDFLAGS="${PULP_SDK_INSTALL}/hero/hero-z-7045/rt_conf.o ${PULP_SDK_INSTALL}/lib/hero-z-7045/rt/crt0.o -nostartfiles -nostdlib -Wl,--gc-sections -T${PULP_SDK_INSTALL}/hero/hero-z-7045/test.ld -T${PULP_SDK_INSTALL}/hero/hero-z-7045/test_config.ld -L${PULP_SDK_INSTALL}/lib/hero-z-7045 -lrt -lrtio -lrt -lgcc -lgomp -lbench -lm"
+# export PULP_HERO_EXTLDFLAGS="${PULP_SDK_INSTALL}/hero/hero-z-7045/rt_conf.o ${PULP_SDK_INSTALL}/lib/hero-z-7045/rt/crt0.o -nostartfiles -nostdlib -Wl,--gc-sections -T${PULP_SDK_INSTALL}/hero/hero-z-7045/test.ld -T${PULP_SDK_INSTALL}/hero/hero-z-7045/test_config.ld -L${PULP_SDK_INSTALL}/lib/hero-z-7045 -lrt -lrtio -lrt -lgcc -lgomp -lbench -lm"
 
 return $RET
 
