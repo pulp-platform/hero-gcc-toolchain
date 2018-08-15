bc087f02ec78cd3e043b57ede9d0f83611038703
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-08-15 09:37:07 +0200

    hero_arm_toolchain_env.sh: enable NEON and half-precision float support
    The Zynq features a Cortex-A9 with full NEON unit and half-precision float
    + VFPv3 FPU.

--- scripts/hero_arm_toolchain_env.sh
+++ scripts/hero_arm_toolchain_env.sh
@@ -21,7 +21,7 @@ HERO_HOST_TARGET=arm-linux-gnueabihf
 HERO_BUILD_TARGET=x86_64-linux-gnu
 HERO_HOST_LINUX_ARCH=arm
 HERO_HOST_GLIBC=glibc-2.21
-HERO_HOST_FPU_CONFIG="--with-fpu=vfpv3-d16 --with-float=hard"
+HERO_HOST_FPU_CONFIG="--with-fpu=neon-fp16 --with-float=hard"
 
 RET=0
 
